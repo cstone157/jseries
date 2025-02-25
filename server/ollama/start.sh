f7d9cfdc6eb5093191afa6bc26321adfa33fb987
cstone157 <culver.stone57@gmail.com>
2025-02-25 13:29:21 -0800

    Cleanup and parse

--- server/ollama/start.sh
+++ server/ollama/start.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+## Make sure our enviroment variables are loaded
+. ~/.profile
+
+## Deploy the actual ollama AI
+/bin/ollama serve &
+
 ## Pull down the lamma 3 AI
 ollama pull llama3
 
 ## Build the ragdemo go enviroment
 cd /ragdemo
 go build
-
-## Deploy the actual ollama AI
-/bin/ollama serve
